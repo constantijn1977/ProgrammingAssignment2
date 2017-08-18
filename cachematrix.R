caf9b2cab37add9947f6e2abe726926faf7a7a38
constantijn1977 <constantijnrijsdijk@hotmail.com>
2017-08-18 13:17:54 +0200

    Update cachematrix.R
    I added a reaction in the script.

--- cachematrix.R
+++ cachematrix.R
@@ -26,6 +26,7 @@ makeCacheMatrix <- function(x = matrix()) {
         list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
+#Nice messages ! (written by Constantijn-Netherlands)
 
 # The following function returns the inverse of the matrix. It first checks if
 # the inverse has already been computed. If so, it gets the result and skips the
